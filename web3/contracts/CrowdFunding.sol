23e2dcff8112fcef4891389c7aa56bab8446ede6
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-06-16 18:07:07 +0530

    final commit

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    struct Campaign{
+    struct Campaign {
         address owner;
         string title;
         string description;
@@ -15,12 +15,14 @@ contract CrowdFunding {
     }
 
     mapping(uint256 => Campaign) public campaigns;
+
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns(uint256) {
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.deadline<block.timestamp, "The deadline should be a date in the future.");
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
@@ -31,30 +33,32 @@ contract CrowdFunding {
 
         numberOfCampaigns++;
 
-        return numberOfCampaigns-1;
+        return numberOfCampaigns - 1;
     }
 
     function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
+
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value : amount}("");
-        if(sent){
-            campaign.amountCollected += amount;
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
 
-    function getDonators(uint256 _id) view public returns(address[] memory, uint256[] memory) {
-        return(campaigns[_id].donators, campaigns[_id].donations);
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
     function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
-        for (uint i=0; i<numberOfCampaigns; i++){
+        for(uint i = 0; i < numberOfCampaigns; i++) {
             Campaign storage item = campaigns[i];
 
             allCampaigns[i] = item;
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -4,10 +4,10 @@ module.exports = {
     version: '0.8.9',
     defaultNetwork: 'goerli',
     networks: {
-      hardhat:{},
-      goerli:{
-        url:'https://rpc.ankr.com/eth_goerli',
-        accounts:[`0x${process.env.PRIVATE_KEY}`],
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
     settings: {
